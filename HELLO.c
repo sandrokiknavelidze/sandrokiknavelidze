dcc93721ea3591d30ebb543d6d3f6772d5e39ee2
SUN <92329133+sandrokiknavelidze@users.noreply.github.com>
2021-10-11 23:10:24 +0400

    Create HELLO.c

---
+++ HELLO.c
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m KIKNA
+- 👀 I’m interested to computer games
+- 🌱 I’m currently learning english and programing
+- 💞️ I’m looking to collaborate on (?)
+- 📫 How to reach the top? idk
+
+<!---
+sandrokiknavelidze/sandrokiknavelidze is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
